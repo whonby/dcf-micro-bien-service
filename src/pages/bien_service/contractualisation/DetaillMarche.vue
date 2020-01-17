3d0f2bf3e678c373a8a01adcda8a12ed4304a894
tony <kadjomathias1994@gmail.com>
2020-01-17 14:36:11 +0000

    mise a jour de vue observation bailleur et ses actions && mutations

--- src/pages/bien_service/contractualisation/DetaillMarche.vue
+++ src/pages/bien_service/contractualisation/DetaillMarche.vue
@@ -1078,6 +1078,33 @@
             </div>
             <div class="modal-body">
                 <form class="form-horizontal">
+
+
+                             <div class="control-group">
+                        <label class="control-label">Document Procedure</label>
+                        <div class="controls">
+                          <select v-model="formObservation.document_procedure_id" class="span">
+                                <option v-for="varText in documentProcedures" :key="varText.id"
+                                        :value="varText.id">{{varText.libelle}}</option>
+                            </select>
+                        
+                        </div>
+                    </div>
+
+                     <div class="control-group">
+                        <label class="control-label">ANO DMP bailleur </label>
+                        <div class="controls">
+                           <select v-model="formObservation.ano_dmp_bailleur_id" class="span">
+                                <option v-for="varText in listeAnoDMPBailleur(marcheid)" :key="varText.id"
+                                        :value="varText.id">{{varText.libelle}}</option>
+                            </select>
+                        </div>
+                    </div>
+
+
+
+
+
                     <div class="control-group">
                         <label class="control-label">Date </label>
                         <div class="controls">
@@ -1138,6 +1165,29 @@
             </div>
             <div class="modal-body">
                 <form class="form-horizontal">
+
+
+                        <div class="control-group">
+                        <label class="control-label">Document Procedure</label>
+                        <div class="controls">
+                          <select v-model="editObservation.document_procedure_id" class="span">
+                                <option v-for="varText in documentProcedures" :key="varText.id"
+                                        :value="varText.id">{{varText.libelle}}</option>
+                            </select>
+                        
+                        </div>
+                    </div>
+
+                     <div class="control-group">
+                        <label class="control-label">ANO DMP bailleur </label>
+                        <div class="controls">
+                           <select v-model="editObservation.ano_dmp_bailleur_id" class="span">
+                                <option v-for="varText in listeAnoDMPBailleur(marcheid)" :key="varText.id"
+                                        :value="varText.id">{{varText.libelle}}</option>
+                            </select>
+                        </div>
+                    </div>
+
                     <div class="control-group">
                         <label class="control-label">Date </label>
                         <div class="controls">
@@ -2940,15 +2990,19 @@ num_courrier:""
 formObservation:{
     date_avis_bail:"",
     avis_bail:"",
-    observations_bail:""
+    observations_bail:"",
+    ano_dmp_bailleur_id:"",
+    document_procedure_id:""
+
 
 },
 
 editObservation:{
     date_avis_bail:"",
     avis_bail:"",
-    observations_bail:""
-
+    observations_bail:"",
+    ano_dmp_bailleur_id:"",
+    document_procedure_id:""
 },
 
 
@@ -3339,8 +3393,10 @@ created() {
                 this.ajouterObseravtionBailleur(this.formObservation)
                 this.formObservation = {
                     date_avis_bail:"",
-                        avis_bail:"",
-                     observations_bail:""
+    avis_bail:"",
+    observations_bail:"",
+    ano_dmp_bailleur_id:"",
+    document_procedure_id:""
 
                 }
 
