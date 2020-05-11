436b52225ce2b8ef515d59adf06621a2451389de
kassi fabrice <kassifabrice2@gmail.com>
2020-05-11 16:08:00 +0000

    maj

--- src/pages/acteurs_depenses/acteurs/Acteurs.vue
+++ src/pages/acteurs_depenses/acteurs/Acteurs.vue
@@ -1,6 +1,3 @@
-
-macheid
-
 <template>
     <div id="">
         <notifications />
@@ -669,7 +666,7 @@ recrutement:""
             // console.log(this.getFonction)
         },
         computed: {
-            ...mapGetters('personnelUA', ["afficheNombrePersonnelRecuActeNormination","fonctionBudgetaire","type_salaries","type_contrats","acte_personnels","type_acte_personnels","fonctions","grades","niveau_etudes",
+            ...mapGetters('personnelUA', ["personnaFonction","afficheNombrePersonnelRecuActeNormination","fonctionBudgetaire","type_salaries","type_contrats","acte_personnels","type_acte_personnels","fonctions","grades","niveau_etudes",
                 "nbr_acteur_actredite_taux","all_acteur_depense","personnaliseActeurFinContrat",
                 "totalActeurEnctivite","totalActeurDepense","totalActeurAccredite","tauxActeurAccredite","totalActeurNonAccredite","personnaliseActeurDepense","affichePersonnelRecuActeNormination"]),
              ...mapGetters("uniteadministrative", ["fonctionsua","servicesua","directions","uniteZones","uniteAdministratives","getPersonnaliseBudgetGeneralParPersonnel"]),
@@ -893,10 +890,10 @@ AffichierElementParent() {
             afficheNomPersonnel() {
       return id => {
         if (id != null && id != "") {
-           const qtereel = this.all_acteur_depense.find(qtreel => qtreel.id == id);
+           const qtereel = this.personnaFonction.find(qtreel => qtreel.acteur_depense.id == id);
 
       if (qtereel) {
-        return qtereel.nom;
+        return qtereel.acteur_depense.nom;
       }
       return 0
         }
@@ -905,10 +902,10 @@ AffichierElementParent() {
  affichePrenomsPersonnel() {
       return id => {
         if (id != null && id != "") {
-           const qtereel = this.all_acteur_depense.find(qtreel => qtreel.id == id);
+           const qtereel = this.personnaFonction.find(qtreel => qtreel.acteur_depense.id == id);
 
       if (qtereel) {
-        return qtereel.prenom;
+        return qtereel.acteur_depense.prenom;
       }
       return 0
         }
@@ -917,10 +914,10 @@ AffichierElementParent() {
     afficheMatriculePersonnel() {
       return id => {
         if (id != null && id != "") {
-           const qtereel = this.all_acteur_depense.find(qtreel => qtreel.id == id);
+           const qtereel = this.personnaFonction.find(qtreel => qtreel.acteur_depense.id == id);
 
       if (qtereel) {
-        return qtereel.matricule;
+        return qtereel.acteur_depense.matricule;
       }
       return 0
         }
