87ed89c69a0b43fb32c9471bc526d2408b16d871
kassi2019 <kassifabrice2@gmail.com>
2020-09-24 10:16:19 +0000

    maj

--- src/pages/parametres_generaux/administratifs/tableauBordGeneral.vue
+++ src/pages/parametres_generaux/administratifs/tableauBordGeneral.vue
@@ -27,58 +27,20 @@
                             
 
 
-                             <ul class="quick-actions" style="margin:0px 180px">
-         <!-- <li class="bg_lb span5"> <a href="#" style="color:black;"> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialTranferst))}}</span><h3>BUDGET TRANSFERT</h3></a> </li> -->
-         <li class="bg_lb span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialTranferst))}}</span><h4>TRANSFERT</h4></a> </li>
-  <li class="bg_lg span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(budgetConsommerTransfert))}}</span><h4>EXECUTE</h4></a> </li>
-    <li class="bg_lo span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(affichierBudgetDisponibleTransfert))}}</span><h4>DISPONIBLE</h4></a> </li>
-    <li class="bg_ly span5"> <a href="#" style="color:black;"><h4>TAUX </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{affichierTauxExecution}}%</span><h4>EXECUTION</h4></a> </li>
-        <!-- <li class="bg_ly span5"> <a href="#" style="color:black;"> <i class="icon-inbox"></i><span class="label label-success" style="font-size:15px">{{formatageSomme(parseFloat(budgetConsommerTransfert))}}</span><h3>BUDGET EXECUTE</h3>  </a> </li> -->
-        <!-- <li class="bg_lo span5"> <a href="#" style="color:black;"> <i class="icon-th"></i><span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(affichierBudgetDisponibleTransfert))}}</span><h3>BUDGET DISPONIBLE</h3> </a> </li>
-        <li class="bg_ls span5"> <a href="#" style="color:black;"> <i class="icon-fullscreen"></i><span class="label label-important" style="font-size:15px">{{affichierTauxExecution}}%</span> <h3>TAUX EXECUTION</h3></a> </li> -->
-  
-      </ul>
+                           
     <div class="row-fluid">
       <div class="widget-box">
         <div class="widget-title bg_lg"><span class="icon"><i class="icon-signal"></i></span>
           <h5>Site Analytics</h5>
         </div>
-        <!-- <div class="widget-content" >
-          <div class="row-fluid">
-            <div class="span9">
-              <div class="chart">
-                  
-              </div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
-              </ul>
-            </div>
-          </div>
-        </div> -->
+        
       </div>
     </div>
                           </div>
 
                           <div id="tab01" class="tab-pane">
                                   <ul class="quick-actions" style="margin:0px 180px">
-             <!-- <li class="bg_lo span3"> <a href="#"> <i class="icon-th-list"></i><span class="label label-important">{{formatageSomme(parseFloat(afficherBudgetInitialPersonnel))}}</span>BUDGET PERSONNEL</a> </li>
-        <li class="bg_ls"> <a href="#"> <i class="icon-tint"></i> <span class="label label-warning"></span> BUDGET EXECUTE</a> </li>
-        <li class="bg_lb"> <a href="#"> <i class="icon-pencil"></i>  <span class="label label-success"></span>BUDGET DISPONIBLE</a> </li>
-        <li class="bg_lg"> <a href="#"> <i class="icon-calendar"></i> <span class="label label-important"> %</span>TAUX EXECUTION</a> </li> -->
-
- <!-- <li class="bg_lb span5"> <a href="#" style="color:black;"> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialPersonnel))}}</span><h3>BUDGET PERSONNEL</h3></a> </li>
-
-        <li class="bg_ly span5"> <a href="#" style="color:black;"> <i class="icon-inbox"></i><span class="label label-success" style="font-size:15px">{{formatageSomme(parseFloat(budgetConsommerPersonnel))}}</span><h3>BUDGET EXECUTE</h3>  </a> </li>
-        <li class="bg_lo span5"> <a href="#" style="color:black;"> <i class="icon-th"></i><span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(affichierBudgetDisponiblePersonnel))}}</span><h3>BUDGET DISPONIBLE</h3> </a> </li>
-        <li class="bg_ls span5"> <a href="#" style="color:black;"> <i class="icon-fullscreen"></i><span class="label label-important" style="font-size:15px">{{affichierTauxExecutionPersonnel}}%</span> <h3>TAUX EXECUTION</h3></a> </li>
-   -->
+           
 
   <li class="bg_lb span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialPersonnel))}}</span><h4>PERSONNEL</h4></a> </li>
   <li class="bg_lg span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(budgetConsommerPersonnel))}}</span><h4>EXECUTE</h4></a> </li>
@@ -90,25 +52,7 @@
         <div class="widget-title bg_lg"><span class="icon"><i class="icon-signal"></i></span>
           <h5>Site Analytics</h5>
         </div>
-        <!-- <div class="widget-content" >
-          <div class="row-fluid">
-            <div class="span9">
-              <div class="chart">
-                  
-              </div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
-              </ul>
-            </div>
-          </div>
-        </div> -->
+        
       </div>
     </div>
                           </div>
@@ -116,64 +60,20 @@
                           
                           <div id="tab4" class="tab-pane active">
                            <ul class="quick-actions" style="margin:0px 180px">
-        <!-- <li class="bg_lb"> <a href="#"> <i class="icon-dashboard"></i> <span class="label label-important">{{formatageSomme(parseFloat(afficherBudgetInitialB))}}</span>BUDGET BIEN & SERVICE</a> </li>
-        <li class="bg_lg span3"> <a href="#"> <i class="icon-signal"></i><span class="label label-warning">{{formatageSomme(parseFloat(afficherBudgetExcuterBienService))}}</span> BUDGET EXECUTE</a> </li>
-        <li class="bg_ly"> <a href="#"> <i class="icon-inbox"></i><span class="label label-success">{{formatageSomme(parseFloat(afficherBudgetInitialB) - (parseFloat(afficherBudgetExcuterBienService)))}}</span> BUDGET DISPONIBLE </a> </li>
-        <li class="bg_lo"> <a href="#"> <i class="icon-th"></i><span class="label label-primary">{{((afficherBudgetInitialB)/(parseFloat(afficherBudgetExcuterBienService)) * 100)}} %</span> <table>TAUX EXECUTION</table></a> </li>
-        -->
-       <!-- <li class="bg_lb span5"> <a href="#" style="color:black;"> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialB))}}</span><h3>BUDGET BIEN & SERVICE</h3></a> </li>
-
-        <li class="bg_ly span5"> <a href="#" style="color:black;"> <i class="icon-inbox"></i><span class="label label-success" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetExcuterBienService))}}</span><h3>BUDGET EXECUTE</h3>  </a> </li>
-        <li class="bg_lo span5"> <a href="#" style="color:black;"> <i class="icon-th"></i><span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(affichierBudgetDisponibleBienService))}}</span><h3>BUDGET DISPONIBLE</h3> </a> </li>
-        <li class="bg_ls span5"> <a href="#" style="color:black;"> <i class="icon-fullscreen"></i><span class="label label-important" style="font-size:15px">{{affichierTauxExecutionBienService}}%</span> <h3>TAUX EXECUTION</h3></a> </li> -->
-  
-
+     
 
   <li class="bg_lb span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialB))}}</span><h4>BIENS & SERVICES</h4></a> </li>
   <li class="bg_lg span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetExcuterBienService))}}</span><h4>EXECUTE</h4></a> </li>
     <li class="bg_lo span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(affichierBudgetDisponibleBienService))}}</span><h4>DISPONIBLE</h4></a> </li>
     <li class="bg_ly span5"> <a href="#" style="color:black;"><h4>TAUX </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{affichierTauxExecutionBienService}}%</span><h4>EXECUTION</h4></a> </li>
-        <!-- <li class="bg_ls"> <a href="#"> <i class="icon-fullscreen"></i> Full width</a> </li>
-        <li class="bg_lo span3"> <a href="#"> <i class="icon-th-list"></i> Forms</a> </li>
-        <li class="bg_ls"> <a href="#"> <i class="icon-tint"></i> Buttons</a> </li>
-        <li class="bg_lb"> <a href="#"> <i class="icon-pencil"></i>Elements</a> </li>
-        <li class="bg_lg"> <a href="#"> <i class="icon-calendar"></i> Calendar</a> </li>
-        <li class="bg_lr"> <a href="#"> <i class="icon-info-sign"></i> Error</a> </li>
-      <li class="bg_lb"> <a href="#"> <i class="icon-dashboard"></i> <span class="label label-important">20</span> My Dashboard </a> </li>
-        <li class="bg_lg span3"> <a href="#"> <i class="icon-signal"></i> Charts</a> </li>
-        <li class="bg_ly"> <a href="#"> <i class="icon-inbox"></i><span class="label label-success">101</span> Widgets </a> </li>
-        <li class="bg_lo"> <a href="#"> <i class="icon-th"></i> Tables</a> </li>
-        <li class="bg_ls"> <a href="#"> <i class="icon-fullscreen"></i> Full width</a> </li>
-        <li class="bg_lo span3"> <a href="#"> <i class="icon-th-list"></i> Forms</a> </li>
-        <li class="bg_ls"> <a href="#"> <i class="icon-tint"></i> Buttons</a> </li>
-        <li class="bg_lb"> <a href="#"> <i class="icon-pencil"></i>Elements</a> </li>
-        <li class="bg_lg"> <a href="#"> <i class="icon-calendar"></i> Calendar</a> </li>
-        <li class="bg_lr"> <a href="#"> <i class="icon-info-sign"></i> Error</a> </li> -->
+    
       </ul>
     <div class="row-fluid">
       <div class="widget-box">
         <div class="widget-title bg_lg"><span class="icon"><i class="icon-signal"></i></span>
           <h5>Site Analytics</h5>
         </div>
-        <!-- <div class="widget-content" >
-          <div class="row-fluid">
-            <div class="span9">
-              <div class="chart">
-                  
-              </div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
-              </ul>
-            </div>
-          </div>
-        </div> -->
+        
       </div>
     </div>
 
@@ -182,17 +82,7 @@
                           
                           <div id="tab1" class="tab-pane">
                           <ul class="quick-actions" style="margin:0px 180px">
-             <!-- <li class="bg_lo span3"> <a href="#"> <i class="icon-th-list"></i><span class="label label-important">{{formatageSomme(parseFloat(afficherBudgetInitialInvetissement))}}</span>BUDGET INVESTISSEMENT</a> </li>
-        <li class="bg_ls"> <a href="#"> <i class="icon-tint"></i> <span class="label label-warning">{{ formatageSomme(parseFloat(afficherBudgetExecutéInvestissement))}}</span> BUDGET EXECUTE</a> </li>
-        <li class="bg_lb"> <a href="#"> <i class="icon-pencil"></i>  <span class="label label-success">{{formatageSomme(parseFloat(afficherBudgetInitialInvetissement) - (parseFloat(afficherBudgetExecutéInvestissement)))}}</span>BUDGET DISPONIBLE</a> </li>
-        <li class="bg_lg"> <a href="#"> <i class="icon-calendar"></i> <span class="label label-important">{{((afficherBudgetInitialInvetissement) / (parseFloat(afficherBudgetExecutéInvestissement)) * 100)}} %</span>TAUX EXECUTION</a> </li> -->
-
- <!-- <li class="bg_lb span5"> <a href="#" style="color:black;"> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialInvetissement))}}</span><h3>BUDGET INVESTISSEMENT</h3></a> </li>
-
-        <li class="bg_ly span5"> <a href="#" style="color:black;"> <i class="icon-inbox"></i><span class="label label-success" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetExecutéInvestissement))}}</span><h3>BUDGET EXECUTE</h3>  </a> </li>
-        <li class="bg_lo span5"> <a href="#" style="color:black;"> <i class="icon-th"></i><span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(affichierBudgetDisponibleInvestissement))}}</span><h3>BUDGET DISPONIBLE</h3> </a> </li>
-        <li class="bg_ls span5"> <a href="#" style="color:black;"> <i class="icon-fullscreen"></i><span class="label label-important" style="font-size:15px">{{affichierTauxExecutionInvestissement}}%</span> <h3>TAUX EXECUTION</h3></a> </li> -->
-  
+          
 
    <li class="bg_lb span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetInitialInvetissement))}}</span><h4>INVESTISSEMENT</h4></a> </li>
   <li class="bg_lg span5"> <a href="#" style="color:black;"><h4>BUDGET </h4> <i class="icon-dashboard"></i> <span class="label label-important" style="font-size:15px">{{formatageSomme(parseFloat(afficherBudgetExecutéInvestissement))}}</span><h4>EXECUTE</h4></a> </li>
@@ -204,25 +94,7 @@
         <div class="widget-title bg_lg"><span class="icon"><i class="icon-signal"></i></span>
           <h5>Site Analytics</h5>
         </div>
-        <!-- <div class="widget-content" >
-          <div class="row-fluid">
-            <div class="span9">
-              <div class="chart">
-                  
-              </div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
-              </ul>
-            </div>
-          </div>
-        </div> -->
+       
       </div>
     </div>
                           </div>
@@ -586,41 +458,5 @@ affichierTauxExecutionInvestissement() {
 }
 </script>
 <style>
-.flex{
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  position: relative;
-}
-.square{
-  width: 50px;
-  height: 50px;
- 
-  color: #FAFAFA;
-  text-align: center;
-  margin-right: 5px;
-  cursor: pointer;
-  line-height: 50px;
-}
-.square:hover{
-  opacity: 0.8;
-}
-.S{
-  background-color:orange;
-}
-.I{
-  background-color:orange;
-}
-.D{
-  background-color:white;
-  color: black;
-}
-.C{
-  background-color:green;
-  color: white;
-}
-.F{
- background-color:green;
-  color: white;
-}
+
 </style>
