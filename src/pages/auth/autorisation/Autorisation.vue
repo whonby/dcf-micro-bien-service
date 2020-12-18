e92c3848c6eb8feafbba2161d9265b5edb591805
Your mahtias <kadjomathias1994@gmail.com>
2020-12-18 16:58:23 +0000

    ok

--- src/pages/auth/autorisation/Autorisation.vue
+++ src/pages/auth/autorisation/Autorisation.vue
@@ -1,90 +1,82 @@
 <template>
     <div>
         <hr>
-      <div class="row-fluid" >
-      <div class="span4">
-        <div class="widget-box">
-          <div class="widget-title"> <span class="icon"> <i class="icon-eye-open"></i> </span>
-            <h5>Browesr statistics</h5>
-          </div>
-          <div class="widget-content nopadding">
-            <table class="table table-bordered" style="width: 100%">
-              <thead>
-                <tr>
-                  <th>Browser</th>
-                  <th>Visits</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Chrome</td>
-                  <td>8850</td>
-                </tr>
-                <tr>
-                  <td>Firefox</td>
-                  <td>5670</td>
-                </tr>
-                <tr>
-                  <td>Internet Explorer</td>
-                  <td>4130</td>
-                </tr>
-                <tr>
-                  <td>Opera</td>
-                  <td>1574</td>
-                </tr>
-                <tr>
-                  <td>Safari</td>
-                  <td>1044</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="span4">
-        <div class="widget-box">
-          <div class="widget-title"> <span class="icon"> <i class="icon-arrow-right"></i> </span>
-            <h5>Website statistics</h5>
-          </div>
-          <div class="widget-content nopadding">
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Site</th>
-                  <th>Visits</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><a href="#">Themeforest.com</a></td>
-                  <td>12444</td>
-                </tr>
-                <tr>
-                  <td><a href="#">Themedesigner.in</a></td>
-                  <td>10455</td>
-                </tr>
-                <tr>
-                  <td><a href="#">Themedesigner.in</a></td>
-                  <td>8455</td>
-                </tr>
-                <tr>
-                  <td><a href="#">Themedesigner.in</a></td>
-                  <td>4456</td>
-                </tr>
-                <tr>
-                  <td><a href="#">Themedesigner.in</a></td>
-                  <td>2210</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      
-    </div>
+       <div id="tab2" class="tab-pane">
+
+                                    <div class="row-fluid">
+                                        <div class="span6" >
+
+
+                                            <div class="widget-box">
+                                                <div class="widget-title">
+                                                    <div class="span6">
+                                                        <span class="icon"> <i class="icon-list"></i> </span>
+                                                        <h5>liste des groupe</h5>
+                                                    </div>
+                  
+
+                                                </div>
+                                                <div class="widget-content">
+                                                    <div  v-for="listeM in getterGroupe" :key="listeM.id" >
+                                                           <div >
+                                                               <p-check class="pretty p-image p-plain "  checked   disabled style=" width: 100%; font-size: 20px !important;" >
+                                                                   <!-- <img slot="extra" class="image" src="../../assets/002.png"> -->
+                                                                  <p style="color: red">{{ listeM.libelle }}</p>
+                                                               </p-check>
+                                                           </div>
+                                                        <div >
+                                                            <p-check class="pretty p-image p-plain "  style=" width: 100%; font-size: 20px !important;"  >
+                                                                <!-- <img slot="extra" class="image" src="../../assets/004.png"> -->
+                                                                
+                                                            </p-check>
+                                                        </div>
+
+
+                                                    </div>
+                                                    
+                                                   </div>
+                                            </div>
+
+
+
+
+
+                                        </div>
+                                        <div class="span6">
+                                            <div class="widget-box">
+                                                <div class="widget-title">
+
+                                                    <div class="span7">
+                                                        <span class="icon">
+                                                    <i class="icon-list"></i> </span> <h5>Unite administrative selectionné <code></code></h5>
+                                                    </div>
+                                                    <div class="span5" align="right" >
+
+                                                        <a href="#myAlert" data-toggle="modal" class="btn btn-primary" >Valider</a></div>
+
+                                                </div>
+                                                <div class="widget-content">
+                                                    <div   >
+
+                                                        <p-check class="pretty p-image p-plain"
+                                                                 style=" width: 100%; font-size: 20px !important;" >
+                                                            <!-- <img slot="extra" class="image" src="../../assets/003.png"> -->
+                                                          
+                                                        </p-check>
+
+                                                    </div>
+                                                    
+                                                     </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
     </div>
 </template>
 <script>
+import {mapGetters} from "vuex";
 export default {
     data(){
         return{
@@ -95,10 +87,16 @@ export default {
 
     },
     computed:{
-
+...mapGetters('Utilisateurs', ['getterGroupe','gettersMenu']),
     },
     methods:{
 
     }
 }
 </script>
+<style scoped>
+.widget-content{
+    margin-left: auto; 
+     margin-right: auto;
+}
+</style>
