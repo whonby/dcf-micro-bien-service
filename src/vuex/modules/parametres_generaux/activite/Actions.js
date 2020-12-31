94de2e425b19d6764f1aab590d83ac478acb6b68
kassi2019 <kassifabrice2@gmail.com>
2020-12-31 15:55:04 +0100

    maj

--- src/vuex/modules/parametres_generaux/activite/Actions.js
+++ src/vuex/modules/parametres_generaux/activite/Actions.js
@@ -123,3 +123,138 @@ export function supprimerPlanActivite({ commit, dispatch }, id) {
             axios.delete('/delete_activites/' + id).then(() => dialog.close())
         })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// get all structure activite
+export  function  getStructureInfrastructure({commit}) {
+    queue.push(() =>  axios.get('/StructureInfrastructure').then((response) => {
+      commit('GET_STRUCTURE_INFRASTRUCTURE', response.data)
+      
+  }).catch(error => console.log(error)))
+  }
+  
+  
+  // ajouter structure activite
+  export  function ajouterStructureInfrastructure({commit}, objetAjoute){
+     asyncLoading( axios.post('/StructureInfrastructure', {
+      niveau:objetAjoute.niveau,
+      libelle: objetAjoute.libelle
+  } )).then(res => {
+          if(res.status == 201){
+              commit('AJOUTER_STRUCTURE_INFRASTRUCTURE', res.data)
+  
+              this.$app.$notify({
+                  title: 'success ',
+                  text: 'Enregistrement effectué avec success !',
+                  type:"success"
+                })
+          
+          }
+      }).catch(error => console.log(error))
+  }
+  
+  // modifier structure activite
+  export function modifierStructureInfrastructure({commit}, activite){
+     asyncLoading( axios.put('/StructureInfrastructure/' + activite.id, {
+      niveau:activite.niveau,
+      libelle: activite.libelle
+  })).then(response => {
+          commit('MODIFIER_STRUCTURE_INFRASTRUCTURE', response.data)
+  
+          this.$app.$notify({
+              title: 'success ',
+              text: 'Modification effectué avec success !',
+              type:"success"
+            })
+      }).catch(error => console.log(error))
+  
+  }
+  
+  
+  // supprimer structureactivite
+  export function supprimerStructureInfrastructure({commit}, id){
+   
+      this.$app.$dialog
+      .confirm("Voulez vouz vraiment supprimer ?.")
+      .then(dialog => {
+         commit('SUPPRIMER_STRUCTURE_INFRASTRUCTURE', id)
+        // // dialog.loading(false) // stops the proceed button's loader
+          axios.delete('/StructureInfrastructure/' + id).then(() => dialog.close() )   
+      })
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  // get all of plan fonctionnelle
+  export function getPlanInfrastructure({ commit }) {
+      queue.push(() => axios.get('/liste_activites').then(tony => {
+          commit('GET_PLAN_INFRASTRUCTURE', tony.data)
+      }).catch(error => console.log(error)))
+  
+  }
+  
+  // ajouter plan fonctionnelle
+  export function ajouterPlanInfrastructure({ commit, dispatch }, objetAjout) {
+      asyncLoading(axios.post('/add_activites', objetAjout)).then(res => {
+          commit('AJOUTER_PLAN_INFRASTRUCTURE', res.data)
+          dispatch('getPlanActivite')
+          this.$app.$notify({
+              title: 'success ',
+              text: 'Enregistrement effectué avec success !',
+              type: "success"
+          })
+      }).catch(error => console.log(error))
+  }
+  // modifier plan fonctionnelle
+  
+  export function modifierPlanInfrastructure({ commit, dispatch }, activite) {
+      asyncLoading(axios.put('/update_activites/' + activite.id, {
+          programme_id: activite.programme_id,
+          numero_ordre: activite.numero_ordre,
+          code: activite.code,
+          libelle: activite.libelle,
+          structure_activites_id: activite.structure_activites_id
+      })).then(res => {
+          commit('MODIFIER_PLAN_INFRASTRUCTURE', res.data)
+          dispatch('getPlanActivite')
+          this.$app.$notify({
+              title: 'success ',
+              text: 'Modification effectué avec success !',
+              type: "success"
+          })
+      }).catch(error => console.log(error))
+  }
+  // supprimer plan fonctionnelle
+  export function supprimerPlanInfrastructure({ commit, dispatch }, id) {
+  
+      this.$app.$dialog
+          .confirm("Voulez vouz vraiment supprimer ?.")
+          .then(dialog => {
+              commit('SUPPRIMER_PLAN_ACTIVITE', id)
+              dispatch('getPlanActivite')
+  
+              // // dialog.loading(false) // stops the proceed button's loader
+              axios.delete('/delete_activites/' + id).then(() => dialog.close())
+          })
+  }
