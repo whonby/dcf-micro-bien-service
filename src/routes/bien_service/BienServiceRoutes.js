0ab1457e7780ef7285a15b04465c641ca4a3fd53
Orlando151091 <bessiorlando@gmail.com>
2021-01-12 11:32:40 +0000

    ol

--- src/routes/bien_service/BienServiceRoutes.js
+++ src/routes/bien_service/BienServiceRoutes.js
@@ -1,6 +1,6 @@
 import TableauBordGestionSibMarche from '../../pages/TableauBord/TableauBordGestionSibMarche.vue'
 import acteDepense from '../../pages/bien_service/acteDepense.vue';
-import ppm_ua from '../../pages/gestionMarche/PpmUa.vue';
+
 
 import organeDecision from "../../pages/bien_service/parametres/organeDecision.vue";
 import naturePrix from "../../pages/bien_service/parametres/naturePrix.vue";
