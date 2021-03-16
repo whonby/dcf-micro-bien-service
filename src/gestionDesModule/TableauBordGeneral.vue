b475254cf6dfabd4726806f7876bb4c28f29c012
whonby <whonby@gmail.com>
2021-03-16 10:38:05 +0000

    Integration

--- src/gestionDesModule/TableauBordGeneral.vue
+++ src/gestionDesModule/TableauBordGeneral.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+   <div class="container-fluid" style="height: 100em;">
         <h1>TABLEAU DE BORD GENERAL</h1>
         <h2>la vue se trouve dans gestion des modules tableau de bord general</h2>
     </div>
