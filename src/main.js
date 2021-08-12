4e81189167ff25bdc63c6ba1e25ff0c3f24eb721
Daouda46 <ouattara.daouda4613@gmail.com>
2021-08-12 11:04:00 +0000

    ok

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 //require('./bootstrap');
 import Vue from 'vue'
-
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
